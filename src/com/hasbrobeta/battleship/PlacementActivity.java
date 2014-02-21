4f974636d2a26adf34a030d1946d6b6f0f013b91
Duncan Smith <duncsmit@umich.edu>
2014-02-21 15:22:51 -0500

    Updated PlacementActivity, all ships are placed with the correct visuals

--- src/com/hasbrobeta/battleship/PlacementActivity.java
+++ src/com/hasbrobeta/battleship/PlacementActivity.java
@@ -3,7 +3,7 @@ package com.hasbrobeta.battleship;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
+import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
@@ -229,6 +229,10 @@ public class PlacementActivity extends Activity {
 			{
 				return R.drawable.carrier_4;
 			}
+			if (bit == 4)
+			{
+				return R.drawable.carrier_5;
+			}
 		}
 		return -1;
 	}
@@ -276,24 +280,49 @@ public class PlacementActivity extends Activity {
 				this.board.squares[coord+j*adjust].shipNum = this.shipType;
 				this.board.squares[coord+j*adjust].shipDirection = this.direction%2;//0 if horizontal, 1 if vertical
 				this.board.squares[coord+j*adjust].shipSegmentNum = j;
-				ImageView shipbit = new ImageView(this);
-				Drawable drawable = getResources().getDrawable(getPic(shipType, i, j));
-				//shipbit.setImageDrawable(drawable);
+//				ImageView shipbit = new ImageView(this);
+//				Drawable drawable = getResources().getDrawable(getPic(shipType, i, j));
+//				shipbit.setImageDrawable(drawable);
 				
-				Matrix mat = new Matrix();
-				Bitmap bMap = BitmapFactory.decodeResource(getResources(),getPic(shipType,i,j));
-				if (direction == 1 || direction == 3)
+//				if (direction == 1 || direction == 3)
+//				{
+//					Matrix matrix = new Matrix();
+//					shipbit.setScaleType(ScaleType.MATRIX);
+//					matrix.postRotate(90f,shipbit.getDrawable().getBounds().width()/2,shipbit.getDrawable().getBounds().height()/2);
+//					shipbit.setImageMatrix(matrix);
+//				}
+				Bitmap bmpOriginal = BitmapFactory.decodeResource(getResources(),getPic(shipType,direction,j));
+				Bitmap bmpunoc = BitmapFactory.decodeResource(getResources(),R.drawable.bg);
+				Bitmap bmResult = Bitmap.createBitmap(bmpOriginal.getWidth(), bmpOriginal.getHeight(), Bitmap.Config.ARGB_8888);
+				Canvas tempCanvas = new Canvas(bmResult);
+				if (direction == 1)
 				{
-					mat.postRotate(90);
+					tempCanvas.rotate(270, bmpOriginal.getWidth()/2, bmpOriginal.getHeight()/2);
 				}
-				Bitmap bMapRotate = Bitmap.createBitmap(bMap, 0, 0,bMap.getWidth(),bMap.getHeight(), mat, true);
-				shipbit.setImageBitmap(bMapRotate);
+				if (direction == 0)
+				{
+					tempCanvas.rotate(180, bmpOriginal.getWidth()/2, bmpOriginal.getHeight()/2);
+				}
+				if (direction == 3)
+				{
+					tempCanvas.rotate(90, bmpOriginal.getWidth()/2, bmpOriginal.getHeight()/2);
+				}
+				tempCanvas.drawBitmap(bmpunoc,0,0,null);
+				tempCanvas.drawBitmap(bmpOriginal, 0, 0, null);
+
+//				if (direction == 1 || direction == 3)
+//				{
+//					mat.postRotate(90);
+//				}
+//				Bitmap bMapRotate = Bitmap.createBitmap(bMap, 0, 0,bMap.getWidth(),bMap.getHeight(), mat, true);
+//				shipbit.setImageBitmap(bMapRotate);
 				
 				
 				GridView g = (GridView) findViewById(R.id.grid_view);
 				LinearLayout nv = (LinearLayout) g.getChildAt(coord+j*adjust);
 				ImageView nvv = (ImageView) nv.findViewById(R.id.usquare);
-				nvv.setImageDrawable(drawable);
+				//nvv.setImageDrawable(drawable);
+				nvv.setImageBitmap(bmResult);
 			}
 			//this looks like only the button call is separate in terms of the logic. Unless there's an unaccounted for
 			//else condition that for a ship type that matches none of them, much of this logic could be called after
@@ -312,32 +341,32 @@ public class PlacementActivity extends Activity {
 			    TextView tv = (TextView) findViewById(R.id.textView5);
 				char l = 'A'; for (int x = 0; x < lcoord; x++) l++;
 				char n = '1'; for (int x = 0; x < ncoord; x++) n++;
-				if (n != ':') tv.setText("Patrol Boat placed starting at "+ l + n + "!");
-				else tv.setText("Patrol Boat placed starting at "+ l + "10!");
+				if (n != ':') tv.setText("Destroyer placed starting at "+ l + n + "!");
+				else tv.setText("Destroyer placed starting at "+ l + "10!");
 			} else if (this.shipType == 2) {
 				Button button = (Button) findViewById(R.id.s3);
 			    button.setEnabled(false);
 			    TextView tv = (TextView) findViewById(R.id.textView5);
 				char l = 'A'; for (int x = 0; x < lcoord; x++) l++;
 				char n = '1'; for (int x = 0; x < ncoord; x++) n++;
-				if (n != ':') tv.setText("Patrol Boat placed starting at "+ l + n + "!");
-				else tv.setText("Patrol Boat placed starting at "+ l + "10!");
+				if (n != ':') tv.setText("Submarine placed starting at "+ l + n + "!");
+				else tv.setText("Submarine placed starting at "+ l + "10!");
 			} else if (this.shipType == 3) {
 				Button button = (Button) findViewById(R.id.b4);
 			    button.setEnabled(false);
 			    TextView tv = (TextView) findViewById(R.id.textView5);
 				char l = 'A'; for (int x = 0; x < lcoord; x++) l++;
 				char n = '1'; for (int x = 0; x < ncoord; x++) n++;
-				if (n != ':') tv.setText("Patrol Boat placed starting at "+ l + n + "!");
-				else tv.setText("Patrol Boat placed starting at "+ l + "10!");
+				if (n != ':') tv.setText("Battleship placed starting at "+ l + n + "!");
+				else tv.setText("Battleship placed starting at "+ l + "10!");
 			} else if (this.shipType == 4) {
 				Button button = (Button) findViewById(R.id.ac5);
 			    button.setEnabled(false);
 			    TextView tv = (TextView) findViewById(R.id.textView5);
 				char l = 'A'; for (int x = 0; x < lcoord; x++) l++;
 				char n = '1'; for (int x = 0; x < ncoord; x++) n++;
-				if (n != ':') tv.setText("Patrol Boat placed starting at "+ l + n + "!");
-				else tv.setText("Patrol Boat placed starting at "+ l + "10!");
+				if (n != ':') tv.setText("Aircraft Carrier placed starting at "+ l + n + "!");
+				else tv.setText("Aircraft Carrier placed starting at "+ l + "10!");
 			}
 			this.shipType = -1;
 			if (this.board.numShipsPlaced == 5) {
