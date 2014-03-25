a02a7cba612fc8fc5094f4bef9a35b40aac37586
Devin Rocco <devjroc@gmail.com>
2014-03-24 21:21:43 -0400

    Added background music, resources, license, implementation

--- src/com/hasbrobeta/battleship/BattleshipMenu.java
+++ src/com/hasbrobeta/battleship/BattleshipMenu.java
@@ -3,6 +3,7 @@ package com.hasbrobeta.battleship;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
@@ -11,11 +12,15 @@ public class BattleshipMenu extends Activity {
 	
 	private SoundPool soundPool;
 	private int soundID;
+	private MediaPlayer mediaPlayer;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_menu);
+		mediaPlayer = MediaPlayer.create(this, R.raw.must_persevere);
+		mediaPlayer.setLooping(true);
+		mediaPlayer.start();
 		soundPool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
 		soundID = soundPool.load(this, R.raw.menu_select, 1);
 	}
